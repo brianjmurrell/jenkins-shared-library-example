1ec31f792a9fb85ae47ced0ff3fc79e9468c4a59
Brian J. Murrell <brian.murrell@intel.com>
2022-07-21 10:50:01 -0400

    Updates

--- src/org/somecompany/IStepExecutor.groovy
+++ src/org/somecompany/IStepExecutor.groovy
@@ -4,6 +4,7 @@ package org.somecompany
  * Interface for calling any necessary Jenkins steps. This will be mocked in unit tests.
  */
 interface IStepExecutor {
+
     int sh(String command)
     void error(String message)
 }
